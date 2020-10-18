94b946c47e17d91662bc0fd225a061b6c3c529c9
anderson-cahet <anderson.cahet@ghe.megaleo.com>
2020-10-18 15:15:16 +0100

    update addEntry
    update icon

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
-import { getMetricMetaInfo } from '../utils/helpers';
+import { View, TouchableOpacity, Text } from 'react-native';
+import { getMetricMetaInfo, timeToString } from '../utils/helpers';
 import UdaciSlider from './UdaciSlider';
 import UdaciSteppers from './UdaciSteppers';
 import DateHeader from './DateHeader';
+import { Ionicons } from '@expo/vector-icons';
+import TextButton from './TextButton';
+
+function SubmitBtn({ onPress }) {
+	return (
+		<TouchableOpacity onPress={onPress}>
+			<Text>SUBMIT</Text>
+		</TouchableOpacity>
+	);
+}
 
 export default class AddEntry extends Component {
 	state = {
@@ -40,8 +50,41 @@ export default class AddEntry extends Component {
 			[metric]: value,
 		}));
 	};
+	submit = () => {
+		const key = timeToString();
+		const entry = this.state;
+
+		// Update Redux
+
+		this.setState(() => ({ run: 0, bike: 0, swim: 0, sleep: 0, eat: 0 }));
+
+		// Navigate to home
+
+		// Save to "DB"
+
+		// Clear local notification
+	};
+	reset = () => {
+		const key = timeToString();
+
+		// Update Redux
+
+		// Route to Home
+
+		// Update "DB"
+	};
 	render() {
 		const metaInfo = getMetricMetaInfo();
+
+		if (this.props.alreadyLogged) {
+			return (
+				<View>
+					<Ionicons name={'ios-happy'} size={100} />
+					<Text>You already logged your information for today.</Text>
+					<TextButton onPress={this.reset}>Reset</TextButton>
+				</View>
+			);
+		}
 
 		return (
 			<View>
@@ -66,6 +109,7 @@ export default class AddEntry extends Component {
 						</View>
 					);
 				})}
+				<SubmitBtn onPress={this.submit} />
 			</View>
 		);
 	}
