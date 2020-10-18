24878bc6c2b2972cfcd763d35cd74c56582ccd8f
anderson-cahet <anderson.cahet@ghe.megaleo.com>
2020-10-18 16:23:49 +0100

    navigation
    using navigation v1 - to update to v5 in a later stage

--- App.js
+++ App.js
@@ -1,19 +1,88 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Platform, StatusBar } from 'react-native';
 import AddEntry from './components/AddEntry';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './reducers';
 import History from './components/History';
+import { TabNavigator } from 'react-navigation';
+import { purple, white } from './utils/colors';
+import { FontAwesome, Ionicons } from '@expo/vector-icons';
+import Constants from 'expo-constants';
+//install expo install expo-constants
+
+import EntryDetail from './components/EntryDetail';
+
+function UdaciStatusBar({ backgroundColor, ...props }) {
+	return (
+		<View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+			<StatusBar translucent backgroundColor={backgroundColor} {...props} />
+		</View>
+	);
+}
+
+const Tabs = TabNavigator(
+	{
+		History: {
+			screen: History,
+			navigationOptions: {
+				tabBarLabel: 'History',
+				tabBarIcon: ({ tintColor }) => <Ionicons name="ios-bookmarks" size={30} color={tintColor} />,
+			},
+		},
+		AddEntry: {
+			screen: AddEntry,
+			navigationOptions: {
+				tabBarLabel: 'Add Entry',
+				tabBarIcon: ({ tintColor }) => <FontAwesome name="plus-square" size={30} color={tintColor} />,
+			},
+		},
+	},
+	{
+		navigationOptions: {
+			header: null,
+		},
+		tabBarOptions: {
+			activeTintColor: Platform.OS === 'ios' ? purple : white,
+			style: {
+				height: 56,
+				backgroundColor: Platform.OS === 'ios' ? white : purple,
+				shadowColor: 'rgba(0, 0, 0, 0.24)',
+				shadowOffset: {
+					width: 0,
+					height: 3,
+				},
+				shadowRadius: 6,
+				shadowOpacity: 1,
+			},
+		},
+	}
+);
+
+const MainNavigator = StackNavigator({
+	Home: {
+		screen: Tabs,
+	},
+	EntryDetail: {
+		screen: EntryDetail,
+		navigationOptions: {
+			headerTintColor: white,
+			headerStyle: {
+				backgroundColor: purple,
+			},
+		},
+	},
+});
 
 export default class App extends React.Component {
 	store = createStore(reducer);
 	render() {
 		return (
 			<Provider store={this.store}>
-				<View style={{ flex: 1 }}>
-					<History />
-				</View>
+				<UdaciStatusBar backgroundColor={purple} barStyle="light-content" />
+				<SafeAreaView style={{ flex: 1 }}>
+					<Tabs />
+				</SafeAreaView>
 			</Provider>
 		);
 	}
