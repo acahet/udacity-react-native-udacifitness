65839d8b9b350ef1175eced9d882d9a8b15779a6
anderson-cahet <anderson.cahet@ghe.megaleo.com>
2020-10-18 15:16:52 +0100

    Changes for API
    add api file
    update addentry for api file

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -6,6 +6,7 @@ import UdaciSteppers from './UdaciSteppers';
 import DateHeader from './DateHeader';
 import { Ionicons } from '@expo/vector-icons';
 import TextButton from './TextButton';
+import { submitEntry, removeEntry } from '../utils/api';
 
 function SubmitBtn({ onPress }) {
 	return (
@@ -60,7 +61,7 @@ export default class AddEntry extends Component {
 
 		// Navigate to home
 
-		// Save to "DB"
+		submitEntry({ key, entry });
 
 		// Clear local notification
 	};
@@ -71,7 +72,7 @@ export default class AddEntry extends Component {
 
 		// Route to Home
 
-		// Update "DB"
+		removeEntry(key);
 	};
 	render() {
 		const metaInfo = getMetricMetaInfo();
@@ -79,7 +80,7 @@ export default class AddEntry extends Component {
 		if (this.props.alreadyLogged) {
 			return (
 				<View>
-					<Ionicons name={'ios-happy'} size={100} />
+					<Ionicons name={'ios-happy-outline'} size={100} />
 					<Text>You already logged your information for today.</Text>
 					<TextButton onPress={this.reset}>Reset</TextButton>
 				</View>
